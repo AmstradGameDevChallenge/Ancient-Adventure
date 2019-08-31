968ac09854bef51eeeb10a1f41cff05e4e226ed6
Miguel Reinoso <miguelreinoso7@gmail.com>
2019-08-31 09:54:32 +0200

    Makefile (bash) for BASIC source code

---
+++ make.sh
@@ -0,0 +1,18 @@
+# Creates a DSK directly from the BASIC source code
+
+#!/bin/bash
+IDSK=$CPCT_PATH/tools/iDSK-0.13/bin/iDSK
+SOURCE=rpg.bas
+TARGET=agc03rpg
+
+# Create DSK
+$IDSK ${TARGET}.dsk -n
+
+# Remove comments
+grep -v "^1 '" $SOURCE > ${TARGET}.bas
+
+# CONVERT TO MSDOS
+unix2dos ${TARGET}.bas
+
+# ADD TO DSK
+$IDSK ${TARGET}.dsk -i ${TARGET}.bas -t 0
